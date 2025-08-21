17ae44ffbec30c2a3cb518287625e7866ea31964
GaiWong <wonggaizhe@gmail.com>
2025-08-21 23:13:05 +0100

    fix: ignore encrypted personal vault
    Syncing personal vault will otherwise cause an error

--- .dotfiles/.local/bin/rclone-bisync-onedrive.sh
+++ .dotfiles/.local/bin/rclone-bisync-onedrive.sh
@@ -2,8 +2,9 @@
 
 notify-send "OneDrive Sync" "Starting rclone bisync..."
 rclone bisync ~/OneDrive onedrive: \
-  --log-file ~/.local/share/rclone-bisync.log \
-  --log-level INFO
+  --exclude "Personal Vault/**" \
+  --verbose \
+  --log-file ~/.cache/rclone/onedrive-sync.log
 
 if [ $? -eq 0 ]; then
   notify-send "OneDrive Sync" "Sync complete ✅"
